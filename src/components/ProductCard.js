cc28fe3bc49d3d8999d91b1d7a265ea8d5fd02f8
sparkmahadi <mahadi.development@gmail.com>
2023-03-31 16:39:37 +0600

    resolved error in productCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
-import { useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
-import { MdDeleteForever } from "react-icons/md";
-import { addToCart, removeFromCart } from "../redux/actions/productAction";
 
 const ProductCard = ({ product }) => {
-  const dispatch = useDispatch();
-  const { pathname } = useLocation();
   return (
-    <div className='shadow-lg relative rounded-3xl border p-3 flex flex-col text-indigo-900'>
-      {pathname.includes("cart") && (
-        <div className='rounded-full grid place-items-center absolute top-2 right-2 bg-indigo-500 text-white h-8 w-8 font-bold '>
-          <p> {product.quantity} </p>
-        </div>
-      )}
+    <div
+      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
+      key={product._id}
+    >
       <div className='h-52 w-52 mx-auto'>
         <img src={product.image} alt={product.model} />
       </div>
@@ -23,42 +15,20 @@ const ProductCard = ({ product }) => {
       <div className=' flex-1'>
         <ul className='space-y-2'>
           {product.keyFeature.map((feature) => {
-            return (
-              <li key={feature} className='text-sm '>
-                {feature}
-              </li>
-            );
+            return <li className='text-sm '>{feature}</li>;
           })}
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        {!pathname.includes("cart") && (
-          <button
-            onClick={() => dispatch(addToCart(product))}
-            className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-          >
-            Add to cart
-          </button>
-        )}
-
-        {!pathname.includes("cart") && (
-          <button
-            title='Add to wishlist'
-            className='bg-indigo-500  py-1 px-2 rounded-full'
-          >
-            <BiListPlus className='text-white' />
-          </button>
-        )}
-        {pathname.includes("cart") && (
-          <button
-            title='Remove'
-            onClick={() => dispatch(removeFromCart(product))}
-            className='flex justify-between px-3 bg-red-500 text-white p-1 rounded-full flex-1'
-          >
-            <p>Remove</p>
-            <MdDeleteForever size='25' />
-          </button>
-        )}
+        <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+          Add to cart
+        </button>
+        <button
+          title='Add to wishlist'
+          className='bg-indigo-500  py-1 px-2 rounded-full'
+        >
+          <BiListPlus className='text-white' />
+        </button>
       </div>
     </div>
   );
---
+++ src/features/filter/filterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    stock: false,
+    brands: [],
+    keyword: "",
+};
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+
+    }
+})
+
+
+export default filterSlice.reducer;
