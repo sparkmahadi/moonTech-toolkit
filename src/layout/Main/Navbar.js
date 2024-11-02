546d26388424c89daa432ac5777ae92f4ec51747
sparkmahadi <mahadi.development@gmail.com>
2024-11-02 16:18:17 +0600

    modify navbar and public

--- src/layout/Main/Navbar.js
+++ src/layout/Main/Navbar.js
@@ -8,14 +8,14 @@ const Navbar = () => {
   return (
     <nav className='h-14 bg-indigo-200 rounded-full m-2 max-w-7xl mx-auto px-5'>
       <ul className='h-full  mx-auto flex justify-between items-center gap-3 font-semibold text-indigo-900'>
-        <h1>Moon Tech</h1>
+      <Link to='/'><h1>Moon Tech</h1></Link>
 
         <li className='flex bg-white mx-auto h-8 w-full max-w-lg  rounded-full pr-3'>
           <input
             className='h-8 rounded-full w-full text-sm border-0 focus:ring-0 outline-none'
             type='text'
             name='search'
-            id='search'
+            id='search' 
           />
           <button>
             <BiSearchAlt />
